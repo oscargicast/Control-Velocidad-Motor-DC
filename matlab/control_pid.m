1cb5d5313e519dc42fa51c322ccb8b03dbdf391a
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2013-12-07 18:09:10 -0500

    Add some images and fix some code

--- matlab/control_pid.m
+++ matlab/control_pid.m
@@ -2,10 +2,10 @@ clear all; close all; clc
 % -------------------------------------------------------------------------
 % Cargando la DATA
 % -------------------------------------------------------------------------
-data41 = load('../data/motor_gate.lvm');
+data_load = load('../data/motor_gate.lvm');
 T = 1/30; % Tiempo de Muestreo 
-y1=data41(:,4);
-u1=data41(:,2);
+y1=data_load(:,4);
+u1=data_load(:,2);
 % -------------------------------------------------------------------------
 % Identificación ARX
 % -------------------------------------------------------------------------
@@ -30,14 +30,16 @@ s1=-zeta*wn+1j*wn*sqrt(1-zeta^2);
 sd=s1;
 pdi=imag(sd); 
 pdr=real(sd);
+
 % ------------------------------------------------------------------------
 % Ploteo de polos y ceros Planta + Controlador
 % ------------------------------------------------------------------------
-plot(real(Pp),imag(Pp),'x','MarkerSize',14) % polos planta
-hold
-plot(real(Zp),imag(Zp),'o','MarkerSize',14) % ceros planta
-plot(pdr,pdi,'xr','MarkerSize',14) % polos deseados
-plot([-18000 1],[0 0],'k') % ejex
+plot(real(Pp),imag(Pp),'xb','MarkerSize',14,'LineWidth',2); % polos planta
+hold on;
+plot(real(Zp),imag(Zp),'ob','MarkerSize',14,'LineWidth',2); % ceros planta
+plot(pdr,pdi,'xg','MarkerSize',14,'LineWidth',2); % polos deseados
+
+plot([-300 1],[0 0],'k') % ejex
 plot([0 0],[-2 8],'k') % ejey
 % ------------------------------------------------------------------------
 % Angulos hacia los polos deseados
@@ -53,10 +55,15 @@ theta_a=0.1*theta_c;
 a=pdr-pdi/tan((180-theta_a)*pi/180);
 theta_b=theta_c; 
 b=pdr-pdi/tan((180-theta_b)*pi/180);
-plot(a,0,'om','MarkerSize',14) % ceros PID (s=-a)
-plot(b,0,'om','MarkerSize',14) % ceros PID (s=-b)
-plot(0,0,'xm','MarkerSize',14) % polos PID
-axis([-2000 200 -5 10])
+plot(a,0,'or','MarkerSize',14,'LineWidth',2) % ceros PID (s=-a)
+plot(b,0,'or','MarkerSize',14,'LineWidth',2) % ceros PID (s=-b)
+plot(0,0,'xr','MarkerSize',14,'LineWidth',2); % polos PID
+
+title('Polos y Ceros de la Planta y Controlador');
+axis([-300 20 -0.5 6.5])
+ylabel('Im'), xlabel('Re');
+
+
 % ------------------------------------------------------------------------
 % Calculo de la Ganancia K (Condicion de Magnitud)
 % ------------------------------------------------------------------------
@@ -68,6 +75,7 @@ Kp=K*abs(a+b);
 Ki=K*abs(a*b); 
 Kd=K;       
 KPID=[Kp Ki Kd];
+
 % ------------------------------------------------------------------------
 % PID No Interactivo GPID(s)=Kp*(1+1/(Ti*s)+Td*s)
 % ------------------------------------------------------------------------
@@ -80,14 +88,17 @@ H=L/(L+1);
 figure
 t=0:0.001:2;
 u=ones(size(t));
-yp=lsim(H,u,t);
-plot(t,u,'k','LineWidth',2)
+yp = lsim(H,u,t);
+yla = lsim(Gp,u,t);
+plot(t,u,'b','LineWidth',2)
 hold
 plot(t,yp,'r','LineWidth',2)
+plot(t,yla,'g','LineWidth',2)
 axis([0 2 0 1.2])
 xlabel('\bf t(seg)')
-h=ylabel('\bf y(t)');
-set(h,'Rotation',pi/2)
+ylabel('\bf y(volts)');
+title('Respuesta del Sistema');
+legend('set point', 'respuesta en lazo cerrado', 'respuesta en lazo abierto')
 grid
 
 % -------------------------------------------------------------------------
@@ -104,7 +115,7 @@ Gdt = Nt/Dt;
 Gdt = subs(Gdt,{'s'},(2*(z-1))/(T*(z+1)));
 Gdt = simplify(Gdt);
 Gdt = vpa(Gdt,4); 
-[NDt DDt] = numden(Gdt);
+[NDt, DDt] = numden(Gdt);
 NDt = sym2poly(NDt);
 DDt = sym2poly(DDt);
 % -------------------------------------------------------------------------
@@ -114,9 +125,8 @@ GDt = tf(NDt,DDt,T);
 printsys(NDt,DDt,'z')
 [Np,Dp]=tfdata(Gp,'v');
 datos=[Np Dp NDt DDt];
-% -------------------------------------------------------------------------
-% Coeficientes para lectura de LabVIEW
-% -------------------------------------------------------------------------
-save '../data/coef_rcnew.lvm' datos -ascii -tabs
+
+disp('Kp Ki Kd:');
+disp(KPID);
 
 
